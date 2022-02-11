2b1f48d0a541393004094cd2dbf43cfc06a54f94
Shucai Xiao <Shucai.Xiao@amd.com>
2022-02-11 09:17:51 -0600

    use __hadd2 for half data type

--- hip/hip_vec_add.cpp
+++ hip/hip_vec_add.cpp
@@ -21,6 +21,24 @@ void vec_add(__half *in1, __half *in2, __half *res, int n)
     return;
 }
 
+__global__ 
+void vec_addH(__half *in1, __half *in2, __half *res, int n)
+{
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+
+    int i = tid;
+    if (i < n)
+    {
+        half2* h1 = (half2*)in1;
+        half2* h2 = (half2*)in2;
+        half2 *r = (half2*)res;
+        r[i] = __hadd2(h1[i], h2[i]);
+    }
+
+    return;
+}
+
+
 __global__ void vec_add(float *in1, float *in2, float *res, int n)
 {
     int tid = blockDim.x * blockIdx.x + threadIdx.x;
@@ -81,7 +99,7 @@ bool hip_vec_addT(const std::vector<T>& in1, const std::vector<T>& in2, std::vec
     hipMemcpy(cu_in2, in2.data(), mem_size, hipMemcpyHostToDevice);
     hipMemcpy(res.data(), in2.data(), mem_size, hipMemcpyDeviceToHost);
 
-    std::size_t block_size = 1024;
+    std::size_t block_size = 512;
     HRTimer timer;
     timer.start();
     vec_add<<<((n - 1) / block_size + 1), block_size>>>(cu_in1, cu_in2, cu_res, n);
@@ -98,9 +116,53 @@ bool hip_vec_addT(const std::vector<T>& in1, const std::vector<T>& in2, std::vec
     return true;
 }
 
+bool hip_vec_add1(const std::vector<__half>& in1, const std::vector<__half>& in2, std::vector<__half>& res) {
+    if (in1.size() != in2.size())
+    {
+        std::cout << "Input vector sizes are different!" << std::endl;
+        return false;
+    }
+
+    using T = __half;
+
+    int n = in1.size();
+    res.resize(n);
+    std::size_t mem_size = n * sizeof(T);
+    T *cu_in1, *cu_in2, *cu_res;
+    hipMalloc((void **)&cu_in1, mem_size);
+    hipMalloc((void **)&cu_in2, mem_size);
+    hipMalloc((void **)&cu_res, mem_size);
+
+    hipMemcpy(cu_in1, in1.data(), mem_size, hipMemcpyHostToDevice);
+    hipMemcpy(cu_in2, in2.data(), mem_size, hipMemcpyHostToDevice);
+    hipMemcpy(res.data(), in2.data(), mem_size, hipMemcpyDeviceToHost);
+
+    std::size_t block_size = 512;
+    HRTimer timer;
+    timer.start();
+    vec_addH<<<((n/2 - 1) / block_size + 1), block_size>>>(cu_in1, cu_in2, cu_res, n);
+    checkHip(hipDeviceSynchronize());
+    timer.stop();
+
+    hipMemcpy(res.data(), cu_res, mem_size, hipMemcpyDeviceToHost);
+    hipFree(cu_in1); hipFree(cu_in2); hipFree(cu_res);
+
+    std::size_t usec = timer.gettime_us();
+    float throughput = 3.0f * mem_size / usec / 1.0e3;
+    std::cout << typeid(T).name() << " vec_add2, time = " << usec << "us, throughput = " << throughput << "GB/s" << std::endl;
+
+    return true;
+}
+
+
 bool hip_vec_add(const std::vector<__half>& in1, const std::vector<__half>& in2, std::vector<__half>& res)
 {
     return hip_vec_addT(in1, in2, res);
+}
+
+bool hip_vec_addH(const std::vector<__half>& in1, const std::vector<__half>& in2, std::vector<__half>& res)
+{
+    return hip_vec_add1(in1, in2, res);
 }
 
 bool hip_vec_add(const std::vector<float>& in1, const std::vector<float>& in2, std::vector<float>& res)
