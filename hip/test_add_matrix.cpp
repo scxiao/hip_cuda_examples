2985aeda7476c1618c4349b9e2bb5f265a3bd147
Shucai Xiao <shucai.xiao@amd.com>
2025-03-03 15:24:25 +0000

    backup changes of test_add_matrix.cpp

--- hip/test_add_matrix.cpp
+++ hip/test_add_matrix.cpp
@@ -11,8 +11,9 @@
 
 using namespace std;
 
-#define BLOCK_M 16
+#define BLOCK_M 64
 #define BLOCK_N 64
+#define PADDING 2
 
 
 template<class T>
@@ -29,35 +30,67 @@ void init_vec(std::vector<T>& vec, std::size_t num)
 }
 
 
-__global__ void kernel_add_matrix(float *a, float *b, float *c, int m, int n)
+// __global__ void kernel_add_matrix(float *a, float *b, float *c, int m, int n)
+// {
+//     extern __shared__ float lds[];
+
+//     float *sa = lds;
+//     float *sb = lds + BLOCK_M * BLOCK_N;
+
+//     // read input matrix to lds
+//     int blk_i = blockIdx.x * BLOCK_M;
+//     int blk_j = blockIdx.y * BLOCK_N;
+
+//     int tid = threadIdx.y;
+//     for (int i = 0; i < BLOCK_M; ++i) {
+//         for (int j = tid; j < BLOCK_N; j += blockDim.y) {
+//             sa[i * BLOCK_N + j] = a[(blk_i + i) * n + blk_j + j];
+//             sb[i * BLOCK_N + j] = b[(blk_i + i) * n + blk_j + j];
+//         }
+//     }
+//     __syncthreads();
+
+//     for (int i = 0; i < BLOCK_M; ++i) {
+//         for (int j = tid; j < BLOCK_N; j += blockDim.y) {
+//             float sum = sa[i * BLOCK_N + j] + sb[i * BLOCK_N + j];
+//             c[(blk_i + i) * n + blk_j + j] = sum;
+//         }
+//     }
+
+//     return;
+// }
+
+__global__ void kernel_add_matrix1(float *a, float *b, float *c, int m, int n)
 {
     extern __shared__ float lds[];
 
     float *sa = lds;
-    float *sb = lds + BLOCK_M * BLOCK_N;
+    float *sb = lds + BLOCK_M * (BLOCK_N + PADDING);
+    int stride_n = (BLOCK_N + PADDING);
 
     // read input matrix to lds
     int blk_i = blockIdx.x * BLOCK_M;
     int blk_j = blockIdx.y * BLOCK_N;
 
     int tid = threadIdx.y;
-    for (int i = 0; i < BLOCK_M; ++i) {
-        for (int j = tid; j < BLOCK_N; j += blockDim.y) {
-            sa[i * BLOCK_N + j] = a[(blk_i + i) * n + blk_j + j];
-            sb[i * BLOCK_N + j] = b[(blk_i + i) * n + blk_j + j];
+    for (int i = tid; i < BLOCK_M; i += blockDim.y) {
+        for (int j = 0; j < BLOCK_N; ++j) {
+            sa[i * stride_n + j] = a[(blk_i + i) * n + blk_j + j];
+            sb[i * stride_n + j] = b[(blk_i + i) * n + blk_j + j];
         }
     }
     __syncthreads();
 
     for (int i = 0; i < BLOCK_M; ++i) {
         for (int j = tid; j < BLOCK_N; j += blockDim.y) {
-            float sum = sa[i * BLOCK_N + j] + sb[i * BLOCK_N + j];
+            float sum = sa[i * stride_n + j] + sb[i * stride_n + j];
             c[(blk_i + i) * n + blk_j + j] = sum;
         }
     }
 
     return;
 }
+
 
 inline
 hipError_t checkHip(hipError_t result)
@@ -86,11 +119,11 @@ bool hip_add_matrix(const std::vector<T>& in1, const std::vector<T>& in2, std::v
     hipMemcpy(cu_in1, in1.data(), mem_size, hipMemcpyHostToDevice);
     hipMemcpy(cu_in2, in2.data(), mem_size, hipMemcpyHostToDevice);
 
-    const std::size_t lds_size = sizeof(T) * BLOCK_M * BLOCK_N;
+    const std::size_t lds_size = 2 * sizeof(T) * BLOCK_M * (BLOCK_N + PADDING);
     // warm up to run the kernel for 10 times
     // for (int i = 0; i < 10; ++i) {
-        hipLaunchKernelGGL(kernel_add_matrix, dim3(m / BLOCK_M, n / BLOCK_N),
-                            dim3(1, BLOCK_N), 2 * lds_size, 0,
+        hipLaunchKernelGGL(kernel_add_matrix1, dim3(m / BLOCK_M, n / BLOCK_N),
+                            dim3(1, BLOCK_N), lds_size, 0,
                             cu_in1, cu_in2, cu_res, m, n);
     // }
     checkHip(hipDeviceSynchronize());
